a04d2f35b72a0a5bce2fe5bca659b505474893ea
Filipe Câncio <filipe.cancio@outlook.com>
2025-08-02 15:15:34 +0000

    aaaaa

--- ios-lab.playground/Contents.swift
+++ ios-lab.playground/Contents.swift
@@ -3,3 +3,24 @@ let name = "Filipe"
 var age = 28
 
 print("Olá Mundo sou o \(name) e tenho \(age) anos.")
+
+var valor1 = 20
+var valor2 = 30
+
+var resultado = valor1 + valor2
+
+print(resultado)
+
+let presName = "Marcelo"
+let presIdade = 34
+let presHobby = "jogar bola"
+
+let apresentacao = """
+Ola tudo bom?
+
+Meu nome: \(presName)
+Minha idade: \(presIdade)
+Meu hobby: \(presHobby)
+"""
+
+print(apresentacao)
