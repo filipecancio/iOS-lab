463112274dcea3b884ba2663fd696263a916574f
Filipe Câncio <filipe.cancio@outlook.com>
2025-08-02 15:30:38 +0000

    aa

--- ios-lab.playground/Contents.swift
+++ ios-lab.playground/Contents.swift
@@ -24,3 +24,31 @@ Meu hobby: \(presHobby)
 """
 
 print(apresentacao)
+
+let MIN_AGE = 18
+let TOO_YOUNG_MESSAGE = "Você é menor de idade. Não pode ainda"
+let EXPECTED_AGE_MESSAGE = "Você é maior de idade. Já pode tirar a carteira"
+let INVALID_AGE_MESSAGE = "Idade inválida."
+
+var currAge = 23
+
+if currAge >= MIN_AGE {
+    print("Você é maior de idade.")
+} else {
+    print("Você é menor de idade.")
+}
+
+let message = currAge >= MIN_AGE ? "Você é maior de idade. Já pode tirar a carteira" : "Você é menor de idade. Não pode ainda"
+
+print("""
+\(message)
+
+""")
+
+currAge = 260
+
+let newMessage = if currAge <= 0 || currAge >= 120  {INVALID_AGE_MESSAGE}
+else if currAge >= MIN_AGE {EXPECTED_AGE_MESSAGE}
+else {EXPECTED_AGE_MESSAGE}
+
+print(newMessage)
